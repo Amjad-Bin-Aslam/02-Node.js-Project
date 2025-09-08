ca63eaa725d915295170018b2cc44f7e754e20a8
Amjad-Bin-Aslam <amjadbinaslam604@gmail.com>
2025-09-09 02:08:35 +0500

    added

--- Blog_Application/index.js
+++ Blog_Application/index.js
@@ -3,9 +3,12 @@ const path = require("path");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 
+const BLog = require("./models/blog")
+
 const userRoute = require('./routes/user');
 const blogRoute = require('./routes/blog');
 const { checkForAuthenticationCookie } = require("./middlewares/authentication");
+const Blog = require("./models/blog");
 
 const app = express();
 const PORT = 8000;
@@ -18,10 +21,13 @@ app.set("views" , path.resolve("./views"));
 app.use(express.urlencoded( { extended: false } ));
 app.use(cookieParser());
 app.use(checkForAuthenticationCookie("token"));
+app.use(express.static(path.resolve('./public')));
 
-app.get("/" , (req , res) => {
+app.get("/" , async (req , res) => {
+    const allBlogs = await Blog.find({});
     res.render("home" , {
         user: req.user,
+        blogs: allBlogs,
     });
 });
 
